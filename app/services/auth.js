ed12e05c8f024af079734d3d0fae436a40cb1123
MarieLouLeJan <marielou.lejan@gmail.com>
2022-11-08 20:54:07 +0100

    update

--- app/services/auth.js
+++ app/services/auth.js
@@ -1,7 +1,12 @@
-export default (req, res, next) => {
+import adressQuery from '../../queries/adressQuery.js';
+
+
+export default async (req, res, next) => {
 
     if (req.session.user) {
         res.locals.user = req.session.user
+        res.locals.user = await adressQuery.getAllAdressesByUser(req.session.user.id)
+
         return next();
     }
     return next()
